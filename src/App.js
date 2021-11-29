59230ed9f63db6a39e9f0b3e80e672e09c38044d
AqeelMalik <maqeel191@gmail.com>
2021-11-29 20:35:23 +0500

    signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import {Login }from './components/Login';
-import{ Signup }from './components/Signup'
-
+import HorizontalLoginForm from './components/Signup'
 function App() {
   return (
     <div className="App">
-       <Login />
-      <Signup />
+      {/* <Signup /> */}
+      <HorizontalLoginForm />
     </div>
   );
 }
